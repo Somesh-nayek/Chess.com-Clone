e251a55902856bdbb74e8ee0686746e0c85d1233
SOMESH NAYEK <myofficialwork2004@gmail.com>
2025-07-02 13:02:28 +0530

    Added api url

--- Chess/src/hooks/useSocket.tsx
+++ Chess/src/hooks/useSocket.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useSocket=()=>{
-    const [socket,setSocket]=useState<WebSocket | null>(null);
-    useEffect(()=>{
-        const ws=new WebSocket("ws://localhost:8080");
-        ws.onopen=()=>setSocket(ws);
-        ws.onclose=()=>setSocket(null);
-        return ()=>{
-            ws.close();
-            setSocket(null);
-        }
-    },[])
-    return socket;
-}
+export const useSocket = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_WSS_SERVER;
+    const ws = new WebSocket(apiUrl);
+    ws.onopen = () => setSocket(ws);
+    ws.onclose = () => setSocket(null);
+    return () => {
+      ws.close();
+      setSocket(null);
+    };
+  }, []);
+  return socket;
+};
